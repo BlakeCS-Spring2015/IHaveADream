a6a073106d323a48b0abf96935f6023536cbcacd
QuinnE <qcellis15@TBSLTMBA593.local>
2015-02-03 11:41:35 -0600

    Put some buttons and image view stuff
    MLK

---
+++ IHAVEADREAM/SecondViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SecondViewController.swift
+//  IHAVEADREAM
+//
+//  Created by qcellis15 on 2/3/15.
+//  Copyright (c) 2015 camccandler15. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBAction func firstPictureButton(sender: AnyObject)
+    {
+        
+    }
+    
+    
+    @IBAction func secondPictureButton(sender: AnyObject)
+        {
+    }
+    
+    
+    @IBAction func thirdPictureButton(sender: AnyObject)
+    {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
